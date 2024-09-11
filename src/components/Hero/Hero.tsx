faa8c4c3524ae73b33bc5d02126a01a54f83ddeb
Doro <60631478+IsidoroAros@users.noreply.github.com>
2024-09-11 12:20:14 +0200

    init commit

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,208 @@
+import styled from "styled-components"
+import bgHero from "../../../public/img/music-festival/bg-hero.png"
+import frameDate from "../../../public/img/music-festival/DCL_MF24_contenedores_desktop 1_transparent.png"
+import { breakpoints } from "../../utils/theme.js"
+
+const Hero = () => {
+  return (
+    <StyledHero>
+      <HeroContainer>
+        <Header>
+          <img
+            src={"../../../public/img/music-festival/header-dcl-music.svg"}
+            alt="header icon"
+            rel="noopener"
+          />
+        </Header>
+        {/* Main Title */}
+        <SaveTheDate>Save the Date</SaveTheDate> {/* New Text */}
+        <Line />
+        <StyledLogo
+          src={"../../../public/img/music-festival/dcl-logo-new.svg"}
+        />
+        <Line />
+        <EventDate>November 20-23</EventDate>
+        {/* Bottom Section */}
+        <BottomSection>
+          <DCLLogoItem
+            src={"../../../public/img/music-festival/decentraland-logo.png"}
+          />
+          <RSVPButton
+            href="https://decentraland.org/events/event/?id=9831c4be-4f94-4bb2-a828-8f48d28a1520"
+            target="_blank"
+            rel="noopener"
+          >
+            <h3>RSVP</h3>
+          </RSVPButton>
+        </BottomSection>
+      </HeroContainer>
+    </StyledHero>
+  )
+}
+
+const SaveTheDate = styled.p`
+  font-family: "InterMedium", sans-serif;
+  font-size: 20px;
+  color: white;
+  text-transform: none;
+  margin-bottom: 24px; /* Add space between this text and the line below */
+  margin-top: 48px; /* Reset margin on top */
+`
+
+const StyledHero = styled.div`
+  position: relative;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  background-image: url(${bgHero});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  overflow: hidden; /* Hide overflow content */
+`
+
+const HeroContainer = styled.div`
+  max-width: 600px;
+  width: 100%;
+  padding: 20px;
+  border: 2.5px solid transparent;
+  position: relative;
+
+  border-image: linear-gradient(90deg, #ff00ff, #00ffcc) 1;
+  background-clip: padding-box;
+  background-color: rgba(0, 0, 0, 0.9);
+  text-align: center;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
+  overflow: hidden; /* Hide overflow content */
+`
+
+const Header = styled.div`
+  position: absolute;
+  width: 100%;
+  top: -5px;
+  left: 0;
+  transform: scale(1.01); /* Scale the image */
+
+  > img {
+    width: 100%;
+  }
+`
+
+const EventDate = styled.p`
+  position: relative;
+  top: -2px;
+  font-family: "InterMedium", sans-serif;
+  font-size: 20px;
+  color: white;
+  text-align: center;
+  width: fit-content;
+  margin: 0 auto;
+  padding: 12px 24px;
+  background-image: url(${frameDate});
+  background-size: cover;
+  background-size: contain;
+  background-position: top center;
+  margin-bottom: 24px;
+  background-repeat: no-repeat;
+`
+
+const BottomSection = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  margin-top: 40px;
+  padding: 0 20px;
+`
+
+const StyledLogo = styled.img`
+  width: 100%;
+  padding: 32px;
+  max-width: 700px;
+  height: auto;
+`
+
+const DCLLogoItem = styled.img`
+  max-width: 100px;
+`
+
+const Line = styled.div`
+  position: relative;
+  width: calc(100% + 40px); /* Extend the line slightly beyond the container */
+  height: 2px;
+  background: linear-gradient(90deg, #ff00ff, #00ffcc); /* Gradient */
+  margin: 0 -20px; /* Shift it left and right by the padding size */
+`
+
+const RSVPButton = styled.a`
+  position: relative;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  text-transform: uppercase;
+  padding: 12px 24px !important;
+  font-family: "InterMedium", sans-serif;
+  font-size: 16px;
+  letter-spacing: 2px;
+  font-weight: 700;
+  color: white;
+  background-color: black; /* Fondo del botón */
+  border-radius: 12px; /* Bordes curvados */
+  padding: 10px;
+  z-index: 1;
+  text-decoration: none;
+  cursor: pointer;
+  text-align: center;
+  line-height: 20px;
+  transition: all 0.4s ease;
+
+  > h3 {
+    z-index: 2;
+  }
+
+  &:hover {
+    box-shadow:
+      0 0 20px rgba(255, 255, 255, 0.4),
+      0 0 10px violet,
+      0 0 5px blue;
+  }
+
+  &::before {
+    z-index: 1;
+    content: "";
+    position: absolute;
+    top: -1.5px;
+    bottom: -1.5px;
+    left: -1.5px;
+    right: -1.5px;
+    background: black;
+    border-radius: 12px; /* Bordes curvados del botón */
+  }
+
+  /* Pseudo-elemento ::after para crear el borde con gradiente */
+  &::after {
+    position: absolute;
+    top: -4px;
+    bottom: -4px;
+    left: -4px;
+    right: -4px;
+    content: "";
+    background: linear-gradient(90deg, #3effa3 0%, #56a7ff 50%, #fb01ff 100%);
+    z-index: -1;
+    border-radius: 16px; /* Bordes curvados del borde (más grande que el botón) */
+    padding: 2px; /* Control del grosor del borde */
+  }
+
+  @media screen and (min-width: ${breakpoints.md}) {
+    font-size: 16px;
+    padding: 12px 24px;
+    height: 36px;
+    margin-top: 0; /* Ajusta márgenes en pantallas más grandes */
+    margin-bottom: 8px;
+  }
+`
+
+// eslint-disable-next-line import/no-default-export
+export default Hero
---
+++ src/utils/theme.js
@@ -0,0 +1,48 @@
+import { darken, lighten, transparentize } from "polished"
+
+const themeConfig = {
+  accent: "#f37877",
+}
+
+const basicPalette = {
+  black: "#000000",
+  white: "#FFFFFF",
+  background: "#040D2D",
+  accent: themeConfig.accent,
+  blueGray: "#B8BEE5",
+  gray: "#CCCCCC",
+}
+
+const expandedPalette = {
+  ...basicPalette,
+  accentLight: () => lighten(0.025, basicPalette.accent),
+  accentExtraLight: () => lighten(0.2, basicPalette.accent),
+  grey600: () => lighten(0.15, basicPalette.black),
+  grey500: () => lighten(0.22, basicPalette.black),
+  grey400: () => lighten(0.35, basicPalette.black),
+  grey300: () => lighten(0.5, basicPalette.black),
+  grey100: () => lighten(0.1, basicPalette.black),
+  blue500: () => darken(0.4, basicPalette.blue),
+  purple500: () => darken(0.4, basicPalette.purple),
+}
+
+const theme = {
+  ...basicPalette,
+  ...expandedPalette,
+}
+
+const breakpoints = {
+  xs: "416px",
+  s: "576px",
+  md: "768px",
+  l: "992px",
+  xl: "1200px",
+  xxl: "1400px",
+}
+
+const boxShadow = {
+  md: `0 0 10px ${transparentize(0.4, basicPalette.black)}`,
+  l: `0 0 30px ${transparentize(0.6, basicPalette.black)}`,
+}
+
+export { theme, breakpoints, boxShadow }
