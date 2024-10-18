60ee9d236e7663c302904d0e6b75c687e77c8fe0
Doro <60631478+IsidoroAros@users.noreply.github.com>
2024-10-18 20:41:32 +0200

    saving progress

--- src/components/Exhibitors/Exhibitors.tsx
+++ src/components/Exhibitors/Exhibitors.tsx
@@ -1,6 +1,80 @@
 import { styled } from "styled-components"
-import exhibitorsData from "../../data/exhibitors.json"
+import AAVEGOTCHI from "../../img/music-festival/AAVEGOTCHI.png"
+import ANY_MAGIK_RPZEG from "../../img/music-festival/ANY-MAGIK-RPZEG.png"
+import CHIBI_CLASH from "../../img/music-festival/CHIBI-CLASH.png"
+import DEFI_KINGDOMS from "../../img/music-festival/DEFI-KINGDOMS.png"
+import KMON from "../../img/music-festival/KMON.png"
+import KNIGHTS_OF_ANTROM from "../../img/music-festival/KNIGHTS-OF-ANTROM.png"
+import LEGIONS_2029 from "../../img/music-festival/LEGIONS-2029.png"
+import LOOTRUSH from "../../img/music-festival/LOOTRUSH.png"
+import MOTODEX from "../../img/music-festival/MOTODEX.png"
+import PLANET_MOJO from "../../img/music-festival/PLANET-MOJO.png"
+import SANDSTORM from "../../img/music-festival/SANDSTORM.png"
+import SPACE_TRAITOR from "../../img/music-festival/SPACE-TRAITOR.png"
 import { Title } from "../Title"
+
+const exhibitorsData = [
+  {
+    title: "AAVEGOTCHI",
+    location: -2.74,
+    image: AAVEGOTCHI,
+  },
+  {
+    title: "DEFI KINGDOMS",
+    location: -6.8,
+    image: DEFI_KINGDOMS,
+  },
+  {
+    title: "KMON",
+    location: -2.74,
+    image: KMON,
+  },
+  {
+    title: "LEGIONS 2029",
+    location: -2.74,
+    image: LEGIONS_2029,
+  },
+  {
+    title: "KNIGHTS OF ANTROM",
+    location: -2.74,
+    image: KNIGHTS_OF_ANTROM,
+  },
+  {
+    title: "ANY MAGIK RPZEG",
+    location: -2.74,
+    image: ANY_MAGIK_RPZEG,
+  },
+  {
+    title: "PLANET MOJO",
+    location: -2.74,
+    image: PLANET_MOJO,
+  },
+  {
+    title: "CHIBI CLASH",
+    location: -2.74,
+    image: CHIBI_CLASH,
+  },
+  {
+    title: "MOTODEX",
+    location: -2.74,
+    image: MOTODEX,
+  },
+  {
+    title: "SPACE TRAITOR",
+    location: -2.74,
+    image: SPACE_TRAITOR,
+  },
+  {
+    title: "LOOTRUSH",
+    location: -2.74,
+    image: LOOTRUSH,
+  },
+  {
+    title: "SANDSTORM",
+    location: -2.74,
+    image: SANDSTORM,
+  },
+]
 
 const Exhibitors = () => {
   return (
