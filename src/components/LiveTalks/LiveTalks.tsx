7827124bce2290b9039b601cafb776ab3b03987b
Doro <60631478+IsidoroAros@users.noreply.github.com>
2024-10-18 20:22:00 +0200

    saving changes

--- src/components/LiveTalks/LiveTalks.tsx
+++ src/components/LiveTalks/LiveTalks.tsx
@@ -1,8 +1,76 @@
 import { styled } from "styled-components"
-import liveTalksData from "../../data/liveTalks.json"
 import bgHero2 from "../../img/music-festival/bg-hero-2.png"
+import imgLiveTalks from "../../img/music-festival/livetalks.png"
 import { breakpoints } from "../../utils/theme"
 import { Title } from "../Title"
+
+const liveTalksData = [
+  {
+    image: imgLiveTalks,
+    title: "Emotes & wearables",
+    description: "Discovering virtual assets for music experience",
+    date: "WEDNESDAY NOV 20",
+    "time-start": "17:00 UTC",
+    "time-end": "9:00 PST",
+  },
+  {
+    image: imgLiveTalks,
+    title: "Label round-table",
+    description: "Increasing authentic engagement in EDM",
+    date: "WEDNESDAY NOV 20",
+    "time-start": "21:00 UTC",
+    "time-end": "14:00 PST",
+  },
+  {
+    image: imgLiveTalks,
+    title: "Tech-driven fan economies",
+    description: "Tools and strategies for independent artists",
+    date: "THURSDAY NOV 21",
+    "time-start": "17:00 UTC",
+    "time-end": "9:00 PST",
+  },
+  {
+    image: imgLiveTalks,
+    title: "Label round-table",
+    description: "New approaches to promoting EDM globally",
+    date: "THURSDAY NOV 21",
+    "time-start": "21:00 UTC",
+    "time-end": "14:00 PST",
+  },
+  {
+    image: imgLiveTalks,
+    title: "Beyong the algorithms",
+    description: "Increasing discoverability trough digital community",
+    date: "FRIDAY NOV 22",
+    "time-start": "17:00 UTC",
+    "time-end": "9:00 PST",
+  },
+  {
+    image: imgLiveTalks,
+    title: "Ai for edm",
+    description: "Shaping tomorrow's intelligent soundscapes",
+    date: "FRIDAY NOV 22",
+    "time-start": "21:00 UTC",
+    "time-end": "14:00 PST",
+  },
+  {
+    image: imgLiveTalks,
+    title: "Final day wrap",
+    description: "Festival highlights with the featured artists (x space)",
+    date: "SATURDAY NOV 23",
+    "time-start": "17:00 UTC",
+    "time-end": "9:00 PST",
+  },
+  {
+    image: imgLiveTalks,
+    title: "Play to the music",
+    description: "Emote raid and parkour rave",
+    date: "SATURDAY NOV 23",
+    "time-start": "21:00 UTC",
+    "time-end": "14:00 PST",
+  },
+]
+
 const LiveTalks = () => {
   return (
     <LiveTalksContainer id="livetalks">
@@ -10,10 +78,7 @@ const LiveTalks = () => {
       <TalksGrid>
         {liveTalksData.map((talk, index) => (
           <TalkCard key={index}>
-            <img
-              src={`../../src/img/music-festival/${talk.image}`}
-              alt={talk.title}
-            />
+            <img src={talk.image} alt={talk.title} />
             <h3>{talk.title}:</h3>
             <p>{talk.description}</p>
             <TimeInfo>
