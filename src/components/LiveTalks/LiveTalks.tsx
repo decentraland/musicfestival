2001aea18bc94f4224ca832472a69ef89efb9b4d
Doro <60631478+IsidoroAros@users.noreply.github.com>
2024-10-19 11:24:22 +0200

    saving changes

--- src/components/LiveTalks/LiveTalks.tsx
+++ src/components/LiveTalks/LiveTalks.tsx
@@ -1,75 +1,8 @@
 import { styled } from "styled-components"
+import { liveTalksData } from "./data"
 import bgHero2 from "../../img/music-festival/bg-hero-2.png"
-import imgLiveTalks from "../../img/music-festival/livetalks.png"
 import { breakpoints } from "../../utils/theme"
 import { Title } from "../Title"
-
-const liveTalksData = [
-  {
-    image: imgLiveTalks,
-    title: "Emotes & wearables",
-    description: "Discovering virtual assets for music experience",
-    date: "WEDNESDAY NOV 20",
-    "time-start": "17:00 UTC",
-    "time-end": "9:00 PST",
-  },
-  {
-    image: imgLiveTalks,
-    title: "Label round-table",
-    description: "Increasing authentic engagement in EDM",
-    date: "WEDNESDAY NOV 20",
-    "time-start": "21:00 UTC",
-    "time-end": "14:00 PST",
-  },
-  {
-    image: imgLiveTalks,
-    title: "Tech-driven fan economies",
-    description: "Tools and strategies for independent artists",
-    date: "THURSDAY NOV 21",
-    "time-start": "17:00 UTC",
-    "time-end": "9:00 PST",
-  },
-  {
-    image: imgLiveTalks,
-    title: "Label round-table",
-    description: "New approaches to promoting EDM globally",
-    date: "THURSDAY NOV 21",
-    "time-start": "21:00 UTC",
-    "time-end": "14:00 PST",
-  },
-  {
-    image: imgLiveTalks,
-    title: "Beyong the algorithms",
-    description: "Increasing discoverability trough digital community",
-    date: "FRIDAY NOV 22",
-    "time-start": "17:00 UTC",
-    "time-end": "9:00 PST",
-  },
-  {
-    image: imgLiveTalks,
-    title: "Ai for edm",
-    description: "Shaping tomorrow's intelligent soundscapes",
-    date: "FRIDAY NOV 22",
-    "time-start": "21:00 UTC",
-    "time-end": "14:00 PST",
-  },
-  {
-    image: imgLiveTalks,
-    title: "Final day wrap",
-    description: "Festival highlights with the featured artists (x space)",
-    date: "SATURDAY NOV 23",
-    "time-start": "17:00 UTC",
-    "time-end": "9:00 PST",
-  },
-  {
-    image: imgLiveTalks,
-    title: "Play to the music",
-    description: "Emote raid and parkour rave",
-    date: "SATURDAY NOV 23",
-    "time-start": "21:00 UTC",
-    "time-end": "14:00 PST",
-  },
-]
 
 const LiveTalks = () => {
   return (
---
+++ src/components/LiveTalks/data.ts
@@ -0,0 +1,68 @@
+import imgLiveTalks from "../../img/music-festival/livetalks.png"
+
+export const liveTalksData = [
+  {
+    image: imgLiveTalks,
+    title: "Emotes & wearables",
+    description: "Discovering virtual assets for music experience",
+    date: "WEDNESDAY NOV 20",
+    "time-start": "17:00 UTC",
+    "time-end": "9:00 PST",
+  },
+  {
+    image: imgLiveTalks,
+    title: "Label round-table",
+    description: "Increasing authentic engagement in EDM",
+    date: "WEDNESDAY NOV 20",
+    "time-start": "21:00 UTC",
+    "time-end": "14:00 PST",
+  },
+  {
+    image: imgLiveTalks,
+    title: "Tech-driven fan economies",
+    description: "Tools and strategies for independent artists",
+    date: "THURSDAY NOV 21",
+    "time-start": "17:00 UTC",
+    "time-end": "9:00 PST",
+  },
+  {
+    image: imgLiveTalks,
+    title: "Label round-table",
+    description: "New approaches to promoting EDM globally",
+    date: "THURSDAY NOV 21",
+    "time-start": "21:00 UTC",
+    "time-end": "14:00 PST",
+  },
+  {
+    image: imgLiveTalks,
+    title: "Beyong the algorithms",
+    description: "Increasing discoverability trough digital community",
+    date: "FRIDAY NOV 22",
+    "time-start": "17:00 UTC",
+    "time-end": "9:00 PST",
+  },
+  {
+    image: imgLiveTalks,
+    title: "Ai for edm",
+    description: "Shaping tomorrow's intelligent soundscapes",
+    date: "FRIDAY NOV 22",
+    "time-start": "21:00 UTC",
+    "time-end": "14:00 PST",
+  },
+  {
+    image: imgLiveTalks,
+    title: "Final day wrap",
+    description: "Festival highlights with the featured artists (x space)",
+    date: "SATURDAY NOV 23",
+    "time-start": "17:00 UTC",
+    "time-end": "9:00 PST",
+  },
+  {
+    image: imgLiveTalks,
+    title: "Play to the music",
+    description: "Emote raid and parkour rave",
+    date: "SATURDAY NOV 23",
+    "time-start": "21:00 UTC",
+    "time-end": "14:00 PST",
+  },
+]
